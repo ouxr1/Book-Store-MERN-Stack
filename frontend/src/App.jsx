eb7a3c461b92c1288e97b252ad1738f772a67c10
oussama <oussamaabd57@gmail.com>
2024-06-15 19:51:26 +0100

    crud react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Routes,Route} from 'react-router-dom';
+import createBooks from './pages/createBooks'
+import deleteBooks from './pages/deleteBooks'
+import editBook from './pages/editBook'
+import showBook from './pages/showBook'
+import Home from './pages/Home'
+
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/books/create' element={<createBook/>}/>
+      <Route path='/books/delete/:id' element={<deleteBooks/>}/>
+      <Route path='/books/edit/:id' element={<editBook />}/>
+      <Route path='/books/details/:id' element={<showBook />}/>
+      <Route path='/' element={<Home />}/>
+    </Routes>
   )
 }
 
